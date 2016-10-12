644f59ccd58515f48dfd4807c99126a7242f8ea6
Gallyam Biktashev <gallyamb@gmail.com>
2016-10-12 15:44:37 +0500

    Update phone-book.js

--- phone-book.js
+++ phone-book.js
@@ -3,11 +3,18 @@
 /**
  * Телефонная книга
  */
-const phoneBook = {}; // Здесь вы храните записи как хотите
-const phoneRegex = /^(\d{3})(\d{3})(\d{2})(\d{2})$/;
 
-function isValidStrings(...strings) {
-    return strings.every(s => typeof s === 'string' && s !== '');
+var phoneBook = {}; // Здесь вы храните записи как хотите
+var phoneRegex = /^(\d{3})(\d{3})(\d{2})(\d{2})$/;
+
+function isValidStrings() {
+    for (var _len = arguments.length, strings = Array(_len), _key = 0; _key < _len; _key++) {
+        strings[_key] = arguments[_key];
+    }
+
+    return strings.every(function (s) {
+        return typeof s === 'string' && s !== '';
+    });
 }
 
 function isCorrectPhone(phone) {
@@ -15,15 +22,13 @@ function isCorrectPhone(phone) {
 }
 
 function getFormattedPhone(phone) {
-    const phonePattern = phone.match(phoneRegex);
+    var phonePattern = phone.match(phoneRegex);
 
-    return `+7 (${phonePattern[1]}) ${phonePattern[2]}-${phonePattern[3]}-${phonePattern[4]}`;
+    return '+7 (' + phonePattern[1] + ') ' + phonePattern[2] + '-' + phonePattern[3] + '-' + phonePattern[4];
 }
 
 function isCorrectInput(phone, name, email) {
-    return isValidStrings(name, phone) &&
-        isCorrectPhone(phone) &&
-        (email === undefined || isValidStrings(email));
+    return isValidStrings(name, phone) && isCorrectPhone(phone) && (email === undefined || isValidStrings(email));
 }
 
 function entryToKey(entry) {
@@ -31,8 +36,7 @@ function entryToKey(entry) {
 }
 
 function entryToString(entry) {
-    return `${entry.name}, ${getFormattedPhone(entry.phone)}` +
-        (entry.hasOwnProperty('email') ? `, ${entry.email}` : '');
+    return entry.name + ', ' + getFormattedPhone(entry.phone) + (entry.hasOwnProperty('email') ? ', ' + entry.email : '');
 }
 
 /**
@@ -48,11 +52,13 @@ exports.isStar = true;
  * @param {String} email
  * @returns {bool}
  */
-exports.add = function (phone, name, email = undefined) {
+exports.add = function (phone, name) {
+    var email = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : undefined;
+
     if (!isCorrectInput(phone, name, email) || exports.find(phone).length !== 0) {
         return false;
     }
-    const entry = { name, phone };
+    var entry = { name: name, phone: phone };
     if (email !== undefined) {
         entry.email = email;
     }
@@ -68,16 +74,18 @@ exports.add = function (phone, name, email = undefined) {
  * @param {String} email
  * @returns {bool}
  */
-exports.update = function (phone, name, email = undefined) {
+exports.update = function (phone, name) {
+    var email = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : undefined;
+
     if (!isCorrectInput(phone, name, email)) {
         return false;
     }
-    const keys = findKeys(phone);
+    var keys = findKeys(phone);
     if (keys.length === 0) {
         return false;
     }
-    keys.forEach(key => {
-        const entry = phoneBook[key];
+    keys.forEach(function (key) {
+        var entry = phoneBook[key];
         Reflect.deleteProperty(phoneBook, key);
         entry.name = name;
         if (email === undefined) {
@@ -105,8 +113,9 @@ function normalizeQuery(query) {
 function findKeys(query) {
     query = normalizeQuery(query);
 
-    return Object.keys(phoneBook)
-        .filter(key => key.indexOf(query) !== -1);
+    return Object.keys(phoneBook).filter(function (key) {
+        return key.indexOf(query) !== -1;
+    });
 }
 
 /**
@@ -115,14 +124,13 @@ function findKeys(query) {
  * @returns {Integer} Count of removed entries
  */
 exports.findAndRemove = function (query) {
-    const keys = findKeys(query);
-    keys.forEach(key => {
+    var keys = findKeys(query);
+    keys.forEach(function (key) {
         Reflect.deleteProperty(phoneBook, key);
     });
 
     return keys.length;
 };
-
 
 /**
  * Поиск записей по запросу в телефонной книге
@@ -130,10 +138,11 @@ exports.findAndRemove = function (query) {
  * @returns {Array<String>}
  */
 exports.find = function (query) {
-    return findKeys(query)
-        .map(key => phoneBook[key])
-        .sort((x, y) => x.name.localeCompare(y.name))
-        .map(entryToString);
+    return findKeys(query).map(function (key) {
+        return phoneBook[key];
+    }).sort(function (x, y) {
+        return x.name.localeCompare(y.name);
+    }).map(entryToString);
 };
 
 /**
@@ -146,13 +155,13 @@ exports.importFromCsv = function (csv) {
     // Парсим csv
     // Добавляем в телефонную книгу
     // Либо обновляем, если запись с таким телефоном уже существует
-    return csv.split('\n').map(line => {
-        const parts = line.split(';');
+    return csv.split('\n').map(function (line) {
+        var parts = line.split(';');
         if (parts.length !== 3 && parts.length !== 2) {
             return false;
         }
-        return exports.add(parts[1], parts[0], parts[2]) ||
-            exports.update(parts[1], parts[0], parts[2]);
-    })
-    .reduce((acc, value) => acc + value, 0);
+        return exports.add(parts[1], parts[0], parts[2]) || exports.update(parts[1], parts[0], parts[2]);
+    }).reduce(function (acc, value) {
+        return acc + value;
+    }, 0);
 };
