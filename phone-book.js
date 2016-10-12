eab40427ba94b7492672a8134257b55714fd88d8
ADruzhinin <mcfriend@yandex.ru>
2016-10-12 17:37:32 +0500

    First try

--- phone-book.js
+++ phone-book.js
@@ -1,50 +1,106 @@
 'use strict';
 
-/**
- * Сделано задание на звездочку
- * Реализован метод importFromCsv
- */
 exports.isStar = true;
 
-/**
- * Телефонная книга
- */
-var phoneBook;
+var phoneBook = [];
 
-/**
- * Добавление записи в телефонную книгу
- * @param {String} phone
- * @param {String} name
- * @param {String} email
- */
+
+function isCorrectPhone(phone) {
+    return typeof phone === 'string' && /^(\d){10}$/.test(phone);
+}
+
+function phoneFormat(phone) {
+    return '+7 (' + phone.slice(0, 3) + ') ' +
+        phone.slice(3, 6) + '-' + phone.slice(6, 8) +
+        '-' + phone.slice(8, 10);
+}
+
+function phoneToPrint(value) {
+    var result = value.name + ', ' + phoneFormat(value.phone);
+    if (value.email) {
+        result += ', ' + value.email;
+    }
+
+    return result;
+}
+
+function searchToQuery(value, query) {
+    return value.phone.indexOf(query) !== -1 ||
+    value.name.indexOf(query) !== -1 || value.email.indexOf(query) !== -1;
+}
+
+function searchPhone(value, phone) {
+    return value.phone === phone;
+}
+
+function getIndexNewPhone(phone) {
+    return phoneBook.findIndex(
+        function (value) {
+            return searchPhone(value, phone);
+        });
+}
+
 exports.add = function (phone, name, email) {
+    if (isCorrectPhone(phone) && typeof name === 'string' &&
+        (typeof email === 'string' || email === undefined) &&
+        email !== '' && name !== '' && getIndexNewPhone(phone) === -1) {
+        phoneBook.push({
+            'name': name,
+            'phone': phone,
+            'email': email
+        });
 
+        return true;
+    }
+
+    return false;
 };
 
-/**
- * Обновление записи в телефонной книге
- * @param {String} phone
- * @param {String} name
- * @param {String} email
- */
 exports.update = function (phone, name, email) {
+    var newIndex = getIndexNewPhone(phone);
+    if (newIndex !== -1) {
+        phoneBook[newIndex].phone = phone;
+        phoneBook[newIndex].name = name;
+        if (email) {
+            phoneBook[newIndex].email = email;
+        } else {
+            phoneBook[newIndex].email = '';
+        }
 
+        return true;
+    }
+
+    return false;
 };
 
-/**
- * Удаление записей по запросу из телефонной книги
- * @param {String} query
- */
 exports.findAndRemove = function (query) {
+    var oldLen = phoneBook.length;
+    phoneBook = phoneBook.filter(
+        function (value) {
+            return !searchToQuery(value, query);
+        });
+    var newLen = phoneBook.length;
 
+    return oldLen - newLen;
 };
 
-/**
- * Поиск записей по запросу в телефонной книге
- * @param {String} query
- */
 exports.find = function (query) {
+    if (!query) {
 
+        return;
+    }
+
+    if (query === '*') {
+
+        return phoneBook.map(phoneToPrint).sort();
+    }
+
+    return phoneBook.filter(
+        function (value) {
+            return searchToQuery(value, query);
+        })
+    .map(phoneToPrint)
+    .sort();
 };
 
 /**
