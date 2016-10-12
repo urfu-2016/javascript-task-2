f4eed41eac3cf4a79fef39bb6b557cdb38f11174
LeMarck <jeysonep@gmail.com>
2016-10-12 09:25:45 +0500

    Update

--- phone-book.js
+++ phone-book.js
@@ -20,7 +20,7 @@ var phoneBook = [];
  * @returns {boolean|*}
  */
 var isValid = function (phone, name) {
-    return /^\d{10}$/gi.test(phone) && typeof name === 'string';
+    return /^\d{10}$/gi.test(phone) && name;
 };
 
 /**
@@ -29,8 +29,6 @@ var isValid = function (phone, name) {
  * @returns {Array.<T>}
  */
 var getRecords = function (query) {
-    query = query || '';
-
     return phoneBook.filter(function (record) {
         return record.phone.indexOf(query) !== -1 ||
             record.name.indexOf(query) !== -1 ||
@@ -108,20 +106,16 @@ exports.findAndRemove = function (query) {
  * @returns {Array.<T>}
  */
 exports.find = function (query) {
-    var records = query === '*' ? getRecords() : getRecords(query);
+    var records = query === '*' ? getRecords('') : getRecords(query);
 
     return records.map(function (record) {
-        var result = [
-            record.name,
+        var phone = /(\d{3})(\d{3})(\d{2})(\d{2})/gi.exec(record.phone);
+        var line = util.format('%s, %s', record.name,
             util.format('+7 (%s) %s-%s-%s',
-                record.phone.substr(0, 3),
-                record.phone.substr(3, 3),
-                record.phone.substr(6, 2),
-                record.phone.substr(8, 2))
-        ].join(', ');
-        result = record.email !== '' ? util.format('%s, %s', result, record.email) : result;
+                phone[1], phone[2], phone[3], phone[4]
+            ));
 
-        return result;
+        return record.email !== '' ? util.format('%s, %s', line, record.email) : line;
     }).sort();
 };
 
@@ -136,10 +130,10 @@ exports.importFromCsv = function (csv) {
     // Добавляем в телефонную книгу
     // Либо обновляем, если запись с таким телефоном уже существует
     var count = 0;
-    var csvArray = csv.split('\n');
-    for (var index = 0; index < csvArray.length; index++) {
-        var data = csvArray[index].split(';');
-        if ((data.length > 1 && data.length < 4 && isValid(data[1], data[0])) &&
+    var csvLines = csv.split('\n');
+    for (var index = 0; index < csvLines.length; index++) {
+        var data = csvLines[index].split(';');
+        if (data.length < 4 &&
             (this.add(data[1], data[0], data[2]) ||
             this.update(data[1], data[0], data[2]))) {
             count++;
