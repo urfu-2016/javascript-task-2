9270c01940f11766cca71808ae2562eafcddce91
Nikolai Bravilov <bravilov@mail.ru>
2016-10-13 19:10:57 +0500

    Каркас программы

--- phone-book.js
+++ phone-book.js
@@ -9,34 +9,76 @@ exports.isStar = true;
 /**
  * Телефонная книга
  */
-var phoneBook;
+var phoneBook = {};
 
 /**
  * Добавление записи в телефонную книгу
  * @param {String} phone
  * @param {String} name
  * @param {String} email
+ * @returns {boolean} - Добавилась ли запись
  */
 exports.add = function (phone, name, email) {
+    var row = { phone: phone, name: name, email: email };
+    if (!isAddingPossible(row)) {
+        return false;
+    }
+    phoneBook[row.phone] = { name: name, email: email };
 
+    return true;
 };
+
+function isAddingPossible(row) {
+    return isInputCorrect(row) && !isRowAlreadyExists(row);
+}
+
+function isInputCorrect(row) {
+    return true;
+}
+
+function isRowAlreadyExists(row) {
+    return true;
+}
 
 /**
  * Обновление записи в телефонной книге
  * @param {String} phone
  * @param {String} name
  * @param {String} email
+ * @returns {boolean} - Обновилась ли запись
  */
 exports.update = function (phone, name, email) {
+    var row = { phone: phone, name: name, email: email };
+    if (!isUpdatingPossible(row)) {
+        return false;
+    }
+    phoneBook[row.phone].name = name;
+    phoneBook[row.phone].email = email;
 
+    return true;
 };
+
+function isUpdatingPossible(row) {
+    return isInputCorrect(row) && isRowAlreadyExists(row);
+}
 
 /**
  * Удаление записей по запросу из телефонной книги
  * @param {String} query
  */
 exports.findAndRemove = function (query) {
+    var phones = findRowsByQuery(query);
+    var rowsDeleted = 0;
+    for (var phone in phones) {
+        if (phoneBook.hasOwnProperty(phone)) {
+            delete phoneBook[phone];
+        } else {
+            throw new Error('Ошибка при составлении списка телефонов для удаления');
+        }
+        rowsDeleted++;
+    }
 
+    return rowsDeleted;
 };
 
 /**
@@ -44,8 +86,21 @@ exports.findAndRemove = function (query) {
  * @param {String} query
  */
 exports.find = function (query) {
-
+    var result = [];
+    var phones = findRowsByQuery(query);
+    for (var phone in phones) {
+        result.push(phone.name + ', ' + formatPhone(phone) + ', ' + phone.email);
+    }
 };
+
+function findRowsByQuery(query) {
+    var result = [];
+    return result;
+}
+
+function formatPhone(phone) {
+    return '+7 (' + (phone.substring(0, 3) + ') ' + phone.substring(3));
+}
 
 /**
  * Импорт записей из csv-формата
