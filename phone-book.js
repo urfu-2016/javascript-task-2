475ccfd605c7aa2a43d4c1efb7e23aa8c460f449
Nikolay Makhalin <makhalin@yandex-team.ru>
2016-10-14 00:09:08 +0500

    fix name check

--- phone-book.js
+++ phone-book.js
@@ -12,7 +12,7 @@ exports.isStar = true;
 var phoneBook = [];
 
 function isValidInput(phone, name) {
-    return /^\d{10}$/g.test(phone) && name;
+    return /^\d{10}$/g.test(phone) && name !== undefined && name.length > 0;
 }
 
 
