f4e780e01ad88b549a93cb2b85edd6f5b6009617
Алексей Котельников <alexeykotelnikov4@gmail.com>
2016-10-13 12:56:10 +0500

    first commit

--- phone-book.js
+++ phone-book.js
@@ -1,15 +1,8 @@
 'use strict';
 
-/**
- * Сделано задание на звездочку
- * Реализован метод importFromCsv
- */
 exports.isStar = true;
 
-/**
- * Телефонная книга
- */
-var phoneBook;
+var phoneBook = {};
 
 /**
  * Добавление записи в телефонную книгу
@@ -17,8 +10,24 @@ var phoneBook;
  * @param {String} name
  * @param {String} email
  */
-exports.add = function (phone, name, email) {
 
+var PHONE_PATTERN = /[0-9]+/;
+
+function isPhoneCorrect(phone) {
+    return PHONE_PATTERN.test(phone) && phone.length === 10;
+}
+
+function isPhoneBookContains(phone) {
+    return Object.keys(phoneBook).indexOf(phone) !== -1;
+}
+
+exports.add = function (phone, name, email) {
+    if (!isPhoneCorrect(phone) || name === undefined || isPhoneBookContains(phone)) {
+        return false;
+    }
+    phoneBook[phone] = {name: name, email: email};
+
+    return true;
 };
 
 /**
@@ -28,7 +37,13 @@ exports.add = function (phone, name, email) {
  * @param {String} email
  */
 exports.update = function (phone, name, email) {
+    if (!isPhoneBookContains(phone) || name === undefined) {
+        return false;
+    }
+    phoneBook[phone].name = name;
+    phoneBook[phone].email = email;
 
+    return true;
 };
 
 /**
@@ -36,15 +51,79 @@ exports.update = function (phone, name, email) {
  * @param {String} query
  */
 exports.findAndRemove = function (query) {
+    var keys = findRecords(query);
+    keys.forEach(function (key) {
+        delete phoneBook[key];
+    });
 
+    return keys.length;
 };
+
+function phoneToString(phone) {
+    return '+7 '.concat('(', phone.slice(0, 3), ') ', phone.slice(3, 6), '-', phone.slice(6, 8), '-', phone.slice(8, 10));
+}
+
+function contains(string, substring) {
+    return string !== undefined && string.indexOf(substring) !== -1;
+}
+
+function isPhoneContainsQuery(phone, query) {
+    var name = phoneBook[phone].name;
+    var email = phoneBook[phone].email;
+
+    return contains(phone, query) || contains(name, query) || contains(email, query);
+}
+
+function findRecords(query) {
+    return Object.keys(phoneBook).filter(function (key) {
+        return isPhoneContainsQuery(key, query);
+    });
+}
+
+function recordToArray(key) {
+    var name = phoneBook[key].name;
+    var phone = key;
+    var email = phoneBook[key].email;
+    if (email === undefined) {
+        return [name, phone];
+    }
+
+    return [name, phone, email];
+}
+
+
+function recordsToString(keys) {
+    return keys
+        .map(recordToArray)
+        .sort(compare)
+        .map(recordArrayToString);
+}
+
+function compare(arr1, arr2) {
+    return arr1[0] > arr2[0] ? 1 : arr1[0] < arr2[0] ? -1 : 0;
+}
+
+function recordArrayToString(arr) {
+    arr[1] = phoneToString(arr[1]);
+
+    return arr.join(', ');
+}
 
 /**
  * Поиск записей по запросу в телефонной книге
- * @param {String} query
+ * @param query
  */
 exports.find = function (query) {
+    var keys = [];
+    if (query === '*') {
+        keys = Object.keys(phoneBook);
+    } else if (query === '') {
+        keys = [];
+    } else {
+        keys = findRecords(query);
+    }
 
+    return recordsToString(keys);
 };
 
 /**
@@ -54,9 +133,26 @@ exports.find = function (query) {
  * @returns {Number} – количество добавленных и обновленных записей
  */
 exports.importFromCsv = function (csv) {
-    // Парсим csv
-    // Добавляем в телефонную книгу
-    // Либо обновляем, если запись с таким телефоном уже существует
+    var records = csv
+        .split('\n')
+        .map(parseCsvLine)
+        .map(addOrUpdateRecord);
 
-    return csv.split('\n').length;
+    return records
+        .filter(function (elem) {
+            return elem;
+        })
+        .length;
 };
+
+function parseCsvLine(line) {
+    return line.split(';');
+}
+
+function addOrUpdateRecord(csvRecord) {
+    var name = csvRecord[0];
+    var phone = csvRecord[1];
+    var email = csvRecord[2];
+
+    return exports.add(phone, name, email) || exports.update(phone, name, email);
+}
