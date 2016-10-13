9df55d7bf7456e2084aada35096437e412d6ddda
AlexSm13 <79068122307@yandex.ru>
2016-10-14 03:32:52 +0500

    PhoneBook

--- phone-book.js
+++ phone-book.js
@@ -9,7 +9,13 @@ exports.isStar = true;
 /**
  * Телефонная книга
  */
-var phoneBook;
+var phoneBook = [];
+
+function inputValidation(phone, name, email) {
+    return typeof phone === "string" && /\d{10}/.test(phone) &&
+    typeof name === "string" && name !== '' && name !== undefined &&
+    (typeof email === "string" || email === undefined);
+}
 
 /**
  * Добавление записи в телефонную книгу
@@ -18,7 +24,21 @@ var phoneBook;
  * @param {String} email
  */
 exports.add = function (phone, name, email) {
+    if (!inputValidation(phone, name, email)) {
+        return false;
+    }
+    for (var i = 0; i < phoneBook.length; i++) {
+        if (phoneBook[i].phone === phone) {
+            return false;
+        }
+    }
+    phoneBook.push({
+        phone: phone,
+        name: name,
+        email: email
+    });
 
+    return true;
 };
 
 /**
@@ -28,7 +48,18 @@ exports.add = function (phone, name, email) {
  * @param {String} email
  */
 exports.update = function (phone, name, email) {
+    if (!inputValidation(phone, name, email)) {
+        return false;
+    }
+    for (var i = 0; i < phoneBook.length; i++) {
+        if (phoneBook[i].phone === phone) {
+            phoneBook[i].name = name;
+            phoneBook[i].email = email;
+            return true;
+        }
+    }
 
+    return false;
 };
 
 /**
@@ -36,7 +67,12 @@ exports.update = function (phone, name, email) {
  * @param {String} query
  */
 exports.findAndRemove = function (query) {
+    var findRecord = exports.find(query);
+    phoneBook = phoneBook.filter(function (array) {
+        return findRecord.indexOf(output(array)) === -1;
+    });
 
+    return findRecord.length;
 };
 
 /**
@@ -44,8 +80,37 @@ exports.findAndRemove = function (query) {
  * @param {String} query
  */
 exports.find = function (query) {
+    if (query === '*') {
+        return phoneBook.map(output).sort();
+    }
+    if (query === '') {
+        return [];
+    }
+    var masRecord = phoneBook.filter(function (arr) {
+        return arr.phone.indexOf(query) !== -1 ||
+            arr.name.indexOf(query) !== -1 ||
+            (arr.email !== undefined && arr.email.indexOf(query) !== -1);
+    });
 
+    return masRecord.map(output).sort();
 };
+
+/**
+ * @return {string}
+ */
+function output(con) {
+    if (con.email != undefined) {
+        var email = ', ' + con.email;
+    } else {
+        email = '';
+    }
+
+    return con.name + ', ' +
+        '+7 (' + con.phone.slice(0, 3) + ') '
+        + con.phone.slice(3, 6) + '-'
+        + con.phone.slice(6, 8) + '-'
+        + con.phone.slice(8) + email;
+}
 
 /**
  * Импорт записей из csv-формата
@@ -57,6 +122,17 @@ exports.importFromCsv = function (csv) {
     // Парсим csv
     // Добавляем в телефонную книгу
     // Либо обновляем, если запись с таким телефоном уже существует
+    var count = 0;
+    var arrayRecord = csv.split('\n');
+    arrayRecord.forEach(function (item) {
+        var masData = item.split(';');
+        if (exports.update(masData[1], masData[0], masData[2])) {
+            count++;
+        }
+        if (exports.add(masData[1], masData[0], masData[2])) {
+            count++;
+        }
+    });
 
-    return csv.split('\n').length;
+    return count;
 };
