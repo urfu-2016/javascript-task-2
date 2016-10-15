7bd009c55f34958fa2cfb5fb984e9596be00f0df
Zeace <pern2500@ya.ru>
2016-10-15 22:07:57 +0500

    первая версия

--- phone-book.js
+++ phone-book.js
@@ -9,7 +9,7 @@ exports.isStar = true;
 /**
  * Телефонная книга
  */
-var phoneBook;
+var phoneBook = [];
 
 /**
  * Добавление записи в телефонную книгу
@@ -18,8 +18,38 @@ var phoneBook;
  * @param {String} email
  */
 exports.add = function (phone, name, email) {
-
+    if (!validPhoneAndName(phone, name)) {
+        return false;
+    }
+    if (!checkExistPhone(phone)) {
+        if (email === undefined) {
+            email = '';
+        }
+        phoneBook.push({'phone': phone, 'name': name, 'email': email});
+        return true;
+    }
+    return false;
 };
+
+function validPhoneAndName(phone, name) {
+    if (typeof(name) !== "string" || phone.length !==  10 || /[^[0-9]/.test(phone)) {
+        return false;
+    }
+    if (typeof(name) !== "string" || name.length === 0) {
+        return false;
+    }
+    return true;
+}
+
+function checkExistPhone(phone) {
+    for (var i = 0; i < phoneBook.length; i++) {
+        var note = phoneBook[i].phone;
+        if (note === phone) {
+            return i;
+        }
+    }
+    return false;
+}
 
 /**
  * Обновление записи в телефонной книге
@@ -28,7 +58,20 @@ exports.add = function (phone, name, email) {
  * @param {String} email
  */
 exports.update = function (phone, name, email) {
-
+    if (!validPhoneAndName(phone, name)) {
+        return false;
+    }
+    if (checkExistPhone(phone)) {
+        phoneBook[checkExistPhone(phone)].phone = phone;
+        phoneBook[checkExistPhone(phone)].name = name;
+        if (email === undefined) {
+            email = '';
+        }
+        phoneBook[checkExistPhone(phone)].email = email;
+    } else {
+        return false;
+    }
+    return true;
 };
 
 /**
@@ -36,7 +79,17 @@ exports.update = function (phone, name, email) {
  * @param {String} query
  */
 exports.findAndRemove = function (query) {
-
+    if (query === undefined || query === '') {
+        return false;
+    }
+    var count = 0;
+    //var sortPhoneBook = phoneBook.sort(sortObj);
+    var indexSearchRes = findSome(query);
+    for (var i = indexSearchRes.length - 1; i > -1; i--) {
+        phoneBook.splice(indexSearchRes[i], 1);
+        count ++;
+    }
+    return count;
 };
 
 /**
@@ -44,9 +97,68 @@ exports.findAndRemove = function (query) {
  * @param {String} query
  */
 exports.find = function (query) {
+    var output = '[' + '\n';
+    if (query == "*") {
+        var sortPhoneBook = phoneBook.sort(sortObj);
+        sortPhoneBook.forEach(function(item) {
+            output += outResult(item);
+        });
+        output = output.substring(0, output.length - 2);
+        output += '\n' + ']';
+        return output;
+    } else if (query === undefined || query === '') {
+        return false;
+    } else {
+        var indexSearchRes = findSome(query);
+        var searchRes = [];
+        for (var i = 0; i < indexSearchRes.length; i++) {
+
+            searchRes.push(phoneBook[indexSearchRes[i]]);
+        }
+        var sortSearchRes = searchRes.sort(sortObj);
+        sortSearchRes.forEach(function(item) {
+            output += outResult(item);
+        });
+        output = output.substring(0, output.length - 2);
+        output += '\n' + ']';
+        return output;
+    }
+
 
 };
 
+function outResult(item) {
+    var output = '';
+    output += '\t\'' + item.name + ', ';
+    output += '+7 (' + item.phone.slice(0,3) + ') ';
+    output += item.phone.slice(3,6) + '-' + item.phone.slice(6,8);
+    output += '-' + item.phone.slice(8,10);
+    if (item.email !== '') {
+        output += ', ' + item.email;
+    }
+    output +='\',\n';
+
+    return output;
+}
+
+
+function findSome(word) {
+    var noteNumbers = [];
+    for (var i = 0; i < phoneBook.length; i++) {
+        var check1 = phoneBook[i]['phone'].indexOf(word);
+        var check2 = phoneBook[i]['name'].indexOf(word);
+        var check3 = phoneBook[i]['email'].indexOf(word);
+        if (check1 !== -1 || check2 !== -1 || check3 !== -1) {
+            noteNumbers.push(i);
+        }
+    }
+    return noteNumbers;
+}
+
+function sortObj(objA, objB) {
+    if (objA.name > objB.name) return 1;
+    if (objA.name < objB.name) return -1;
+}
 /**
  * Импорт записей из csv-формата
  * @star
@@ -57,6 +169,18 @@ exports.importFromCsv = function (csv) {
     // Парсим csv
     // Добавляем в телефонную книгу
     // Либо обновляем, если запись с таким телефоном уже существует
-
-    return csv.split('\n').length;
+    var newNote = csv.split('\n');
+    var countImport = 0;
+    var notes = [];
+    for (var i = 0; i < newNote.length; i++) {
+        notes = newNote[i].split(';');
+        if (!exports.update(notes[1], notes[0], notes[2])) {
+            if (exports.add(notes[1], notes[0], notes[2])) {
+                countImport++;
+            }
+         } else {
+            countImport++;
+        }
+    }
+    return countImport;
 };
