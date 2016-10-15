0e9d30334eb107213588228e8c75ba552d0ed165
Андрей Трефилов < >
2016-10-15 08:38:11 +0500

    Здача книги

--- phone-book.js
+++ phone-book.js
@@ -18,8 +18,30 @@ var phoneBook;
  * @param {String} email
  */
 exports.add = function (phone, name, email) {
+    checkPhoneBook();
+    if (!name || !phone
+    || phone.length !== 10) {
 
+        return false;
+    }
+    if (this.find(phone).length) {
+        return false;
+    }
+    phoneBook.push({phone: phone, name: name, email: email});
+    if (!email) {
+        phoneBook[phoneBook.length - 1].email = '';
+    }
+    phoneBook.sort(function (a, b) {
+        return Number(a.phone) - Number(b.phone);
+    });
+    return true;
 };
+
+function checkPhoneBook() {
+    if (!phoneBook) {
+        phoneBook = [];
+    }
+}
 
 /**
  * Обновление записи в телефонной книге
@@ -28,6 +50,19 @@ exports.add = function (phone, name, email) {
  * @param {String} email
  */
 exports.update = function (phone, name, email) {
+    if (Number(phone) && phone.length === 10
+    && email) {
+        phoneBook.forEach(function (contact) {
+            if (contact.phone === phone) {
+                contact.name = name;
+                contact.email = email;
+
+                return true;
+            }
+        });
+    }
+
+    return false;
 
 };
 
@@ -36,7 +71,12 @@ exports.update = function (phone, name, email) {
  * @param {String} query
  */
 exports.findAndRemove = function (query) {
+    var deleteCollection = this.find(query);
+    phoneBook = phoneBook.filter(function (contacts) {
+        return deleteCollection.indexOf(finishFormat(contacts)) === -1;
+    });
 
+    return deleteCollection.length;
 };
 
 /**
@@ -44,8 +84,38 @@ exports.findAndRemove = function (query) {
  * @param {String} query
  */
 exports.find = function (query) {
+    if (!query.length) {
+        return ;
+    }
+    if (query === '*') {
+        return phoneBook;
+    }
+    var response = [];
+    phoneBook.forEach(function (contact) {
+        if (contact.phone.search(query) === 0
+        || contact.name.search(query) === 0
+        || contact.email.search(query) === 0) {
+            response.push(contact);
+        }
+    });
 
+    return response.map(finishFormat).sort();
 };
+
+function finishFormat(contact) {
+    var emailFormat = ', ';
+    if (contact.email) {
+        emailFormat += contact.email;
+    }
+    var phone = contact.phone;
+
+    return contact.name + ', ' +
+        '+7 (' + phone.slice(0, 3) + ') ' +
+        phone.slice(3, 6) + '-' +
+        phone.slice(6, 8) + '-' +
+        phone.slice(8, 10) +
+        emailFormat;
+}
 
 /**
  * Импорт записей из csv-формата
