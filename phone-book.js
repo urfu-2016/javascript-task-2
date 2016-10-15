5b9fc964af936073d02ca768fa021d732138674c
Nazarova Galina <galkadaw@gmail.com>
2016-10-15 16:42:00 +0500

    Редактор телефонной книги

--- phone-book.js
+++ phone-book.js
@@ -9,43 +9,115 @@ exports.isStar = true;
 /**
  * Телефонная книга
  */
-var phoneBook;
+var phoneBook = [];
 
 /**
  * Добавление записи в телефонную книгу
  * @param {String} phone
  * @param {String} name
  * @param {String} email
+ * @returns {Boolean} удалось добавить запись или нет
  */
 exports.add = function (phone, name, email) {
+    if (phone.search(/\d{10}/) < 0 || name === undefined) {
+        return false;
+    }
+    var isExistPhone = false;
+    for (var i = 0; i < phoneBook.length; ++i) {
+        if (phoneBook[i].phone === phone) {
+            return false;
+        }
+    }
+    if (!isExistPhone) {
+        phoneBook.push(
+            {
+                phone: phone,
+                name: name,
+                email: email
+            }
+        );
+    }
 
+    return !isExistPhone;
 };
+
 
 /**
  * Обновление записи в телефонной книге
  * @param {String} phone
  * @param {String} name
  * @param {String} email
+ * @returns {Boolean} удалось обновить запись или нет
  */
 exports.update = function (phone, name, email) {
+    if (phone.search(/\d{10}/) < 0 || name === undefined) {
+        return false;
+    }
 
+    for (var j = 0; j < phoneBook.length; ++j) {
+        if (phoneBook[j].phone === phone) {
+            phoneBook[j].name = name;
+            phoneBook[j].email = email;
+
+            return true;
+        }
+    }
+
+    return false;
 };
+
 
 /**
  * Удаление записей по запросу из телефонной книги
  * @param {String} query
+ * @returns {Number} число удаленных записей
  */
 exports.findAndRemove = function (query) {
+    var countOfDelete = 0;
+    var k = 0;
+    while (k < phoneBook.length) {
+        if (phoneBook[k].phone.indexOf(query) > -1 || phoneBook[k].name.indexOf(query) > -1 ||
+            phoneBook[k].email !== undefined && phoneBook[k].email.indexOf(query) > -1) {
+            phoneBook.splice(k, 1);
+            countOfDelete ++;
+        } else {
+            k++;
+        }
+    }
 
+    return countOfDelete;
 };
 
 /**
  * Поиск записей по запросу в телефонной книге
  * @param {String} query
+ * @returns {Array} result of search
  */
 exports.find = function (query) {
+    if (query === undefined || query === '') {
+        return [];
+    }
+    var findResult = [];
+    for (var k = 0; k < phoneBook.length; ++k) {
+        if (isFoundNote(phoneBook[k], query)) {
+            findResult.push(phoneBook[k].name + ', ' + getFormatPhone(phoneBook[k].phone) +
+                ((phoneBook[k].email !== undefined) ? ', ' + phoneBook[k].email : ''));
+        }
+    }
 
+    return findResult.sort();
 };
+
+function isFoundNote(note, query) {
+    return query === '*' || note.phone.indexOf(query) > -1 || note.name.indexOf(query) > -1 ||
+        note.email !== undefined && note.email.indexOf(query) > -1;
+}
+
+function getFormatPhone(phone) {
+    return '+7 (' + phone.substr(0, 3) + ') ' +
+    phone.substr(3, 3) + '-' + phone.substr(6, 2) + '-' +
+    phone.substr(8, 2);
+}
 
 /**
  * Импорт записей из csv-формата
@@ -57,6 +129,19 @@ exports.importFromCsv = function (csv) {
     // Парсим csv
     // Добавляем в телефонную книгу
     // Либо обновляем, если запись с таким телефоном уже существует
+    var csvLines = csv.split('\n');
+    var csvPartsOfData = [];
+    var countOfAddStrings = 0;
+    for (var m = 0; m < csvLines.length; m++) {
+        csvPartsOfData = csvLines[m].split(';');
 
-    return csv.split('\n').length;
+        if (exports.add(csvPartsOfData[1], csvPartsOfData[0], csvPartsOfData[2]) ||
+            exports.update(csvPartsOfData[1], csvPartsOfData[0], csvPartsOfData[2])) {
+            countOfAddStrings++;
+        }
+    }
+
+    return countOfAddStrings;
 };
+
+
