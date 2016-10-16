aba6d1f1de8ea9e4c337817480729770dfad30d6
fng3r <dinvk@mail.ru>
2016-10-16 19:48:35 +0500

    Update phone-book.js

--- phone-book.js
+++ phone-book.js
@@ -6,13 +6,13 @@ exports.isStar = true;
 var phoneBook = [];
 
 function isValidPhone(phone) {
-    var validPhone = /\d{10}/;
+    var validPhone = /^\d{10}&/;
 
     return validPhone.test(phone) && typeof phone === 'string';
 }
 
 function isValidEmail(email) {
-    var validEmail = /\w+@\w+-?\w+.\w{2,255}/;
+    var validEmail = /^\w+@\w+-?\w+.\w{2,255}$/;
 
     return validEmail.test(email) || email === undefined;
 }
@@ -121,6 +121,11 @@ exports.find = function (query) {
 };
 
 exports.importFromCsv = function (csv) {
+    if (typeof csv !== 'string') {
+
+        return 0;
+    }
+
     return csv.split('\n').reduce(function (acc, item) {
         var args = item.split(';');
         var phone = args[1];
