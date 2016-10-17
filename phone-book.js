f2d9762d7edbed76585ab70bb359eec9701f49a7
ByzovGeorgy <sonissd@gmail.com>
2016-10-18 01:08:29 +0500

    ????

--- phone-book.js
+++ phone-book.js
@@ -37,7 +37,7 @@ exports.add = function (phone, name, email) {
 };
 
 function checkPhoneFormat(phone) {
-    return phone && phone.length === 10 && !phone.match(/[^0-9]/);
+    return phone.length === 10 && !phone.match(/[^0-9]/);
 }
 
 /**
