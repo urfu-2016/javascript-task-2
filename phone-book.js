cafbf7ca91f54a7fc16df283ec4ee1a6092fb5d4
Александр Соловьев <alexsprint27@mail.ru>
2016-10-17 11:51:50 +0600

    Update phone-book.js

--- phone-book.js
+++ phone-book.js
@@ -51,7 +51,7 @@ function getCorrectData(phone, name, email) {
             return false;
         }
         data[correctPhone].name = name;
-        if (email || email !== '' && typeof email === 'string') {
+        if (email !== '' && typeof email === 'string') {
             data[correctPhone] = addEntryToData(
                 data[correctPhone], email, 'email', /.*/);
         }
