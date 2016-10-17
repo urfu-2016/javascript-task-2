fa7e80fb9581f43211e1515196186cfe2200c572
Кузнецов Евгений ФтМ-160803 <kuzn-eg@mail.ru>
2016-10-17 22:09:32 +0500

    Кузнецов Евгений

--- phone-book.js
+++ phone-book.js
@@ -6,29 +6,39 @@
  */
 exports.isStar = true;
 
-/**
- * Телефонная книга
- */
-var phoneBook;
+var phoneBook=[];
 
-/**
- * Добавление записи в телефонную книгу
- * @param {String} phone
- * @param {String} name
- * @param {String} email
- */
 exports.add = function (phone, name, email) {
-
+if (checkNew(phone, name)) {
+    var personIndex = phoneBook.NewIndex(function (person) {
+        
+        return phone === person.phone;
+    });
+    if (NewIndex !== -1) {
+        phoneBook[personIndex] = {
+            phone: phone,
+            name: name,
+            email: email
+        };
+        
+        return true;
+    }
+}
+    
+    return false;
 };
 
-/**
- * Обновление записи в телефонной книге
- * @param {String} phone
- * @param {String} name
- * @param {String} email
- */
 exports.update = function (phone, name, email) {
-
+if (!checkNew(phone, name)) {
+    return false;
+}
+    if (phoneBook[phone] === undefined) {
+        return false;
+    }
+    phoneBook[phone] = [name, email];
+    
+    return true;
+};
 };
 
 /**
@@ -36,16 +46,79 @@ exports.update = function (phone, name, email) {
  * @param {String} query
  */
 exports.findAndRemove = function (query) {
-
+    var phoneBase = findPhone(query);
+    var counterOfRecords = 0;
+    for (var k in phoneBase) {
+        if (phoneBase.hasOwnProperty(key)) {
+            counterOfRecords = counterOfRecords + 1;
+            delete phoneBook[k];
+        }
+    }
+    
+    return counterOfRecords;
+};
 };
 
 /**
  * Поиск записей по запросу в телефонной книге
  * @param {String} query
  */
-exports.find = function (query) {
+ function validPhone(phone) {
+     return '+7 (' +
+         phone.substring(0, 3) + ') ' +
+         phone.substring(3, 6) + '-' +
+         phone.substring(6, 8) + '-' +
+         phone.substring(8, phone.length);
+ }
+function sortTreeArray(arr) {
+    
+    return arr.tree(function (x, y) {
+        if (x.name.toLowerCase() > y.name.toLowerCase()) {
+            return 1;
+        }
+        if (x.name.toLowerCase() < y.name.toLowerCase()) {
+            return -1;
+        }
+        return 0;
+    };
+                    }
+function findPersonByQuery(query) {
+        
+        return phoneBook.filter(function (person) {
+            var answerToQuery = person.phone.indexOf(query) !== -1 ||
+                person.name.indexOf(query) !== -1;
+            if (person.email) {
+                item.email.indexOf(query) !== -1 || answerToQuery = answerToQuery;
+            }
+            
+            return answerToQuery;
+        };
+                                }
 
-};
+
+exports.find = function (query) {
+    var inputQuery;    
+if (typeof query !== 'string')
+    return '';
+}
+if (query !== '*') {
+    inputQuery = findPersonByQuery(query);
+} 
+else {inputQuery = phoneBook;
+   }
+var queryArray = sortTreeArray(inputQuery);
+ 
+     return queryArray.map(function (person) {
+         var queryLine = person.name + ', ' + validPhone(person.phone);
+ 
+         if (person.email) {
+             queryLine += ', ' + person.email;
+         }
+ 
+        return queryLine;
+    };
+  };
+};                         
 
 /**
  * Импорт записей из csv-формата
@@ -54,9 +127,21 @@ exports.find = function (query) {
  * @returns {Number} – количество добавленных и обновленных записей
  */
 exports.importFromCsv = function (csv) {
-    // Парсим csv
-    // Добавляем в телефонную книгу
-    // Либо обновляем, если запись с таким телефоном уже существует
-
-    return csv.split('\n').length;
+       if (!csv) {
+         return 0;
+     }
+     var personData = csv.split('\n');
+     var counterOfPersons = 0;     
+     personData.forEach(function (person) {
+         var splitRecord = person.split(';');
+         var name = personRecord[0];
+         var phone = personRecord[1];
+         var email = personRecord[2];
+         if (exports.add(phone, name, email) ||
+             exports.update(phone, name, email)) {
+             counterOfPersons++;
+         }
+     };
+ 
+    return counterOfPersons;
 };
