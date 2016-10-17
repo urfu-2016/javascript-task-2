84787d16ed16b563a17308c694261546d3332b21
Степан <stepanus97@yandex.ru>
2016-10-17 23:32:50 +0500

    Update phone-book.js

--- phone-book.js
+++ phone-book.js
@@ -24,7 +24,7 @@ function isConstains(i, object) {
 }
 
 function isFound(i, q) {
-    if (i._email === q || i._phone === q || i._name === q) {
+    if (i._email.indexOf(q) !== -1 || i._phone.indexOf(q) !== -1 || i._name.indexOf(q) !== -1) {
         return true;
     }
 
