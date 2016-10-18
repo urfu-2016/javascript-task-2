523953abab7124dbeb6eb5d03049f8816ea3258b
AnastasiaBabushkina <nastena.babushkina.96@mail.ru>
2016-10-18 23:17:34 +0500

    тесты

--- phone-book.js
+++ phone-book.js
@@ -10,7 +10,7 @@ function strAndEmp(phone, name) {
 
 function check(phone, name) {
     var isStringP = typeof Number(phone) !== 'number';
-    var isPositive = Number(phone) < 0 || phone === '';
+    var isPositive = phone === '';
     var reg = !/^[0-9]{10}$/.test(phone) || isPositive;
     if (phone.length !== 10 || strAndEmp(phone, name) || isStringP || reg) {
 
