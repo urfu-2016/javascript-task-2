e4c6871c3365d4660c865b20e8de1d2acf77bb0e
Варюхина Арина КН-302 <arinavaruhina@gmail.com>
2016-10-18 22:15:41 +0500

    Добавлены проверки

--- phone-book.js
+++ phone-book.js
@@ -31,7 +31,7 @@ function checkExist(phone) {
 function testPhone(phone) {
     var regvalidPhone = /\d{10}/;
 
-    return regvalidPhone.test(phone);
+    return regvalidPhone.test(phone) && phone.length === 10;
 }
 
 
