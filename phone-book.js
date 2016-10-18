9c3c0ff66079ec6d2ea0afc415e4ba3111fe19d4
Boo1ka <egor.boo1ka@gmail.com>
2016-10-18 13:06:41 +0500

    New findAndRemove.

--- phone-book.js
+++ phone-book.js
@@ -72,28 +72,18 @@ exports.update = function (phone, name, email) {
 
 
 exports.findAndRemove = function (query) {
-    var result = 0;
-    if ((!query) || (typeof(query) !== 'string')) {
-        return result;
-    }
     var finds = exports.find(query);
-    phoneBook = phoneBook.filter(isMatch);
-    function isMatch(person) {
-        for (var i = 0; i < finds.length; i++) {
-            if (person.phone === finds[i].phone) {
-                return false;
-            }
-        }
-
-        return true;
+    if (finds.length !== 0) {
+        phoneBook = phoneBook.filter(function (person) {
+            return finds.indexOf(correctOutput(person)) === -1;
+        });
     }
-    result = finds.length;
 
-    return result;
+    return finds.length;
 };
 exports.find = function (query) {
     var result = [];
-    if ((!query) || (typeof(query) !== 'string')) {
+    if (!query) {
         return result;
     }
     if (query === '*') {
@@ -103,7 +93,7 @@ exports.find = function (query) {
     .map(correctOutput);
 
     function findEntry(person) {
-        if (person.email !== undefined) {
+        if (person.email) {
             return (person.name.indexOf(query) !== -1) ||
         (person.phone.indexOf(query) !== -1) ||
         (person.email.indexOf(query) !== -1);
@@ -122,22 +112,22 @@ exports.find = function (query) {
 
         return 0;
     }
-    function correctPhoneOutput(phone) {
-        return '+7 (' + phone.slice(0, 3) + ') ' +
-        phone.slice(3, 6) + '-' + phone.slice(6, 8) + '-' +
-        phone.slice(8, 10);
-    }
-
-    function correctOutput(person) {
-        if (person.email !== undefined) {
-            return person.name + ', ' + correctPhoneOutput(person.phone) + ', ' + person.email;
-        }
-
-        return person.name + ', ' + correctPhoneOutput(person.phone);
-    }
 
     return result;
 };
+function correctOutput(person) {
+    if (person.email) {
+        return person.name + ', ' + correctPhoneOutput(person.phone) + ', ' + person.email;
+    }
+
+    return person.name + ', ' + correctPhoneOutput(person.phone);
+}
+
+function correctPhoneOutput(phone) {
+    return '+7 (' + phone.slice(0, 3) + ') ' +
+        phone.slice(3, 6) + '-' + phone.slice(6, 8) + '-' +
+        phone.slice(8, 10);
+}
 
 /**
  * Импорт записей из csv-формата
