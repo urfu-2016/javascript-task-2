713bba4209ca8b154723998ae74f299201a3c8cc
Pogromistka <int.sai@yandex.by>
2016-10-19 23:01:09 +0500

    Добавлен вариант решения (испр. 20)

--- phone-book.js
+++ phone-book.js
@@ -220,12 +220,12 @@ function correctEmail(str) {
     if (str === undefined || typeof(str) !== 'string') {
         return false;
     }
-    // str = removeSpace(str);
+    str = /^[a-z0-9_\.-]+@[a-z0-9-]+\.([a-z]{2,4}\.)?[a-z]{2,4}$/i.test(email);
 
-    // return (str.lenght !== 0 && str.indexOf('@') !== -1);
-    str = str.match(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+.\.[a-zA-Z]{2,}$/);
+    return (str.lenght !== 0 && str.indexOf('@') !== -1);
+    // str = str.match(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+.\.[a-zA-Z]{2,}$/);
 
-    return str !== null || str !== '';
+    // return str !== null || str !== '';
 }
 
 /**
