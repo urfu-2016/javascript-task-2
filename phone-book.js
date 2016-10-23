f775452108b187cbecda5f9d294e97b1c431ad0a
AnastasiaBabushkina <nastena.babushkina.96@mail.ru>
2016-10-23 16:59:23 +0500

    поправила поиск индекса

--- phone-book.js
+++ phone-book.js
@@ -113,6 +113,7 @@ exports.findAndRemove = function (query) {
     resultIndex.push(findIndex(phoneBook.phone, query));
     resultIndex.push(findIndex(phoneBook.name, query));
     resultIndex.push(findIndex(phoneBook.email, query));
+    console.log(resultIndex);
     if (resultIndex !== []) {
         var arr = addIndexArray(resultIndex);
         phoneBook = addNewBook(arr);
@@ -126,8 +127,10 @@ exports.findAndRemove = function (query) {
 function findIndex(arr, str) {
     var result = [];
     for (var i = 0; i < arr.length; i++) {
-        if (arr[i].toLowerCase().indexOf(str.toLowerCase()) !== -1) {
-            result.push(i);
+        if (typeof arr[i] !== 'undefined') {
+            if (arr[i].toLowerCase().indexOf(str.toLowerCase()) !== -1) {
+                result.push(i);
+            }
         }
     }
 
