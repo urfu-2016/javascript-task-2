1ffe7988252b68284df7c8fa903fad039725589f
Кулаев Игорь <kulaevmath@gmail.com>
2016-10-27 22:56:00 +0500

    Update phone-book.js

--- phone-book.js
+++ phone-book.js
@@ -108,10 +108,10 @@ function phoneBookToCustomView(book) {
         '-' +
         splittedPhone[8] +
         splittedPhone[9];
-        if (((book[i])[2] === '') || ((book[i])[2] === undefined)) {
+        if (((book[i])[2] === '') || ((book[i])[2] === undefined))
             bookWithCustomPhones.push([(book[i])[1], customPhone]);
-        }
-        else bookWithCustomPhones.push([(book[i])[1], customPhone, (book[i])[2]]);
+        else
+            bookWithCustomPhones.push([(book[i])[1], customPhone, (book[i])[2]]);
     }
     var sortedBookWithCustomPhones = bookWithCustomPhones.sort();
     var customBook = [];
